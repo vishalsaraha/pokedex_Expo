dd00d2ebe8d81a32b06da85b4d982aad36e29979
vishalsaraha <vishalsaran23@gmail.com>
2025-11-19 13:14:48 +0530

    the calling of component library is important

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
-import { ScrollView } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, View, Text } from "react-native";
 
 export default function Index() {
+  const[pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
     //fetch pokemons
@@ -12,7 +13,8 @@ export default function Index() {
     try{
       const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=20");
       const data = await response.json();
-      console.log(data);
+      setPokemons(data.results);
+
     } catch(e) {
       console.log(e);
     }
@@ -20,7 +22,16 @@ export default function Index() {
 
   return (
     <ScrollView>
-      
+      {
+        pokemons.map((pokemon) => (
+
+          <View key={pokemon.name}>
+            <Text>
+              {pokemon.name}
+            </Text>
+          </View>
+
+        ))}
     </ScrollView>
   );
 }
